279591a345fa9a0be44548eb08bfee0e5fc8c17e
monta0315 <monta0315@docomo.ne.jp>
2021-08-20 13:21:57 +0900

    config add

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDJYU1FcV5A9PNH8hlemHDBJhyLovFTsuw",
+  authDomain: "gorilla-afb22.firebaseapp.com",
+  projectId: "gorilla-afb22",
+  storageBucket: "gorilla-afb22.appspot.com",
+  messagingSenderId: "882584136311",
+  appId: "1:882584136311:web:4c3c7a0c7c92efdf2e7472",
+};
+
+export default firebaseConfig;
